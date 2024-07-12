788011499bde1f9bf964d616d12e6dcbd346f14e
masumweb <engineermasum64@gmail.com>
2024-07-12 14:53:42 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//STICKY MENU
+$(".js--services-section").waypoint(function(direction){
+    if (direction == "down") {
+        $("nav").addClass("sticky");
+    } else {
+        $("nav").removeClass("sticky");
+    }
+});
+
+
+    // MIXITUP (PORTFOLIO SECTION)
+    var mixer = mixitup('.container');
+    
+});
+
